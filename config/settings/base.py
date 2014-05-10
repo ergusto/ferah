bb4e2befa821fedbb746fdf28fc2058cd0826ff1
Fergus Ruston <fergus.ruston@gmail.com>
2014-05-10 22:15:16 +0100

    heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -107,3 +107,21 @@ STATIC_URL = '/static/'
 
 
 # Vendor
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
