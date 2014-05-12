463372bb44f59f9d8074c9622abfea6a8222dd1d
Fergus Ruston <fergus.ruston@gmail.com>
2014-05-12 19:21:08 +0100

    stuff

--- apps/tags/forms.py
+++ apps/tags/forms.py
@@ -14,3 +14,17 @@ class SimpleTagForm(ModelForm):
 		super(SimpleTagForm, self).__init__(*args, **kwargs)
 		self.fields['title'].label = 'Tag text'
 		self.fields['title'].help_text = 'Enter tags separated by a comma.'
+
+	def clean_title(self):
+		title = self.cleaned_data['title']
+		slug = slugify(title)
+		if len(slug) == 0:
+			raise forms.ValidationError("Please enter a valid title.")
+		try:
+			convo = Conversation.objects.get(slug=slug)
+		except Conversation.DoesNotExist:
+			return title
+		else:
+			if self.instance and convo == self.instance:
+				return title
+			raise forms.ValidationError("A conversation with that title already exists.")
