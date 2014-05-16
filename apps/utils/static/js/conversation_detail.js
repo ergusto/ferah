2c464c4ad1c1d5c38215e29cdfb92467f12330c8
Fergus Ruston <fergus.ruston@gmail.com>
2014-05-16 10:49:47 +0100

    tag stuff now working again

--- apps/utils/static/js/conversation_detail.js
+++ apps/utils/static/js/conversation_detail.js
@@ -1,37 +1,20 @@
-;(function($, window, document, undefined) {
-	'use strict';
-
-	var pluginName = 'jangoTags',
-		defaults = {
-			handlebarsTemplate: null,
-			appendTo: null,
-			prependTo: null,
-		};
-
-	function JangoTags(form, settings) {
-		this.form = $(form);
-
-		this.settings = settings;
-		this._defaults = defaults;
-		this._name = pluginName;
-		this.init();
-	}
-
-	JangoTags.prototype = {
-
-		init: function() {
-			this.primeEventListeners();
-		},
-
-	};
-
-	$.fn[ pluginName ] = function(options) {
-		return this.each(function() {
-			var settings = $.extend( {}, defaults, options);
-			if ( !$.data( this, "plugin_" + pluginName ) ) {
-				$.data( this, "plugin_" + pluginName, new JangoTags(this, settings) );
+$(document).ready(function() {
+	$(document).on('click', '.js-tag-remove', function(event){
+		event.preventDefault();
+		var anchor = $(this);
+		var title = anchor.parent().text().trim();
+		$.ajax({
+			url: anchor.attr('href'),
+			type: 'POST',
+			data: {'title': title},
+			dataType: 'json',
+			success: function(response) {
+				anchor.closest('.tag_list__item').remove();
+			},
+			error: function(response, textStatus, jqXHR) {
+				console.log(response);
+				console.log(response);
 			}
-		}, options);
-	};
-
-})(jQuery, window, document);
+		});
+	});
+});
