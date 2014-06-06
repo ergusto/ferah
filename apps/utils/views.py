4d36871ce0dd15edc9c80a99511f86f38bfbf73b
Fergus Ruston <fergus.ruston@gmail.com>
2014-06-06 19:01:12 +0100

    login view

--- apps/utils/views.py
+++ apps/utils/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
