a871ed545ee3c4114ca934279dd456c9435fa8be
Fergus Ruston <fergus.ruston@gmail.com>
2014-06-06 18:38:09 +0100

    migrations

---
+++ apps/profiles/migrations/0003_auto__del_profile.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'Profile'
+        db.delete_table(u'profiles_profile')
+
+
+    def backwards(self, orm):
+        # Adding model 'Profile'
+        db.create_table(u'profiles_profile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name='profile', unique=True, to=orm['auth.User'])),
+        ))
+        db.send_create_signal(u'profiles', ['Profile'])
+
+
+    models = {
+        
+    }
+
+    complete_apps = ['profiles']
